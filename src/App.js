48277c4f50436aadc2f58304dc828037ca93fb65
Timur <hasan13@me.com>
2017-10-26 23:05:18 +0300

    туц
    тут

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 
-import React from "react";
+import { Component } from 'react';
 import ReactDOM from "react-dom";
 import './index.css';
 
-ReactDOM.render(
-  <h1>Hello, world!!!!!</h1>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
